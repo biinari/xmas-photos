53e8a7442be7a6d0f91f203525e1a228bfc2c337
Bill Ruddock <bill.ruddock@gmail.com>
2011-09-26 21:24:06 +0100

    fix with width and height

--- python/process.py
+++ python/process.py
@@ -2,15 +2,19 @@ import os, sys
 import shutil
 import Image, ImageFile
 
+width = 2272
+height = 1704
+
 try:
-    maskimage = Image.open('mask/2272x1704.png')
+    maskimage = Image.open('mask/{0}x{1}.png'.format(width, height))
 except IOError:
-    print "cannot open mask file"
+    print "cannot open mask file {0}x{1}.png".format(width, height)
 
 for infile in os.listdir('infiles/'):
+    white = Image.new('RGBA', (width, height), (255,255,255,255))
     try:
         inimage = Image.open('infiles/' + infile)
     except IOError:
         print "Cannot open", infile
-    maskimage.paste(inimage)
-    maskimage.save('outfiles/' + infile, 'PNG')
+    white.paste(inimage, (0, 0, width, height), maskimage)
+    white.save('outfiles/' + infile, 'PNG')
