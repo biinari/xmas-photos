02c50dfb3d37f115c4d01b23857e0e4df3db8c12
Bill Ruddock <bill.ruddock@gmail.com>
2011-09-26 22:44:17 +0100

    get size from photo

--- python/process.py
+++ python/process.py
@@ -2,19 +2,19 @@ import os, sys
 import shutil
 import Image, ImageFile
 
-width = 2272
-height = 1704
-
-try:
-    maskimage = Image.open('mask/{0}x{1}.png'.format(width, height))
-except IOError:
-    print "cannot open mask file {0}x{1}.png".format(width, height)
-
 for infile in os.listdir('infiles/'):
-    white = Image.new('RGBA', (width, height), (255,255,255,255))
     try:
-        inimage = Image.open('infiles/' + infile)
+        photo = Image.open('infiles/' + infile)
     except IOError:
         print "Cannot open", infile
-    white.paste(inimage, (0, 0, width, height), maskimage)
-    white.save('outfiles/' + infile, 'PNG')
+    (width, height) = photo.size
+    try:
+        mask = Image.open('mask/{0}x{1}.png'.format(width, height))
+    except IOError:
+        print "cannot open mask file {0}x{1}.png".format(width, height)
+    try:
+        base = Image.open('base/{0}x{1}.png'.format(width, height));
+    except IOError:
+        print "Cannot open base file {0}x{1}.png".format(width, height)
+    base.paste(photo, (0, 0, width, height), mask)
+    base.save('outfiles/' + infile, 'PNG')
