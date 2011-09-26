45870138955a4bffc65f3876bc2ec883376bcf01
Bill Ruddock <bill.ruddock@gmail.com>
2011-09-26 23:04:37 +0100

    add breaks

--- python/process.py
+++ python/process.py
@@ -7,14 +7,17 @@ for infile in os.listdir('infiles/'):
         photo = Image.open('infiles/' + infile)
     except IOError:
         print "Cannot open", infile
+        break
     (width, height) = photo.size
     try:
         mask = Image.open('mask/{0}x{1}.png'.format(width, height))
     except IOError:
         print "cannot open mask file {0}x{1}.png".format(width, height)
+        break
     try:
         base = Image.open('base/{0}x{1}.png'.format(width, height));
     except IOError:
         print "Cannot open base file {0}x{1}.png".format(width, height)
+        break
     base.paste(photo, (0, 0, width, height), mask)
     base.save('outfiles/' + infile, 'PNG')
