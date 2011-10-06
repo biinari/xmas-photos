8c130935375ae102b63402b0d8990c8ca9a1ed7a
Bill Ruddock <bill.ruddock@gmail.com>
2011-10-07 00:48:40 +0100

    create mask

--- python/process.py
+++ python/process.py
@@ -1,31 +1,46 @@
 import os, sys
 import shutil
 import Image, ImageFile
+import math
+
+def maskPixel(row, col, width, height):
+    exp = 24
+    val = (1 - pow((2.0 * col / width - 1), exp)) * (1 - pow((2.0 * row / height - 1), exp))
+    return int(math.floor(255 * val))
 
 def createMask(width, height):
     mask = Image.new('RGBA', (width, height))
+    data = []
+    for row in range(height):
+        for col in range(width):
+            data.append(maskPixel(row, col, width, height))
+    data = zip(data, data, data, map(lambda x: 255, range(height*width)))
+    mask.putdata(data)
     mask.save('mask/{0}x{1}.png'.format(width, height))
 
 def createBase(width, height):
     base = Image.new('RGBA', (width, height), (255,255,255,255))
     base.save('base/{0}x{1}.png'.format(width, height))
 
-for infile in os.listdir('infiles/'):
-    try:
-        photo = Image.open('infiles/' + infile)
-    except IOError:
-        print "Cannot open", infile
-        break
-    (width, height) = photo.size
-    try:
-        mask = Image.open('mask/{0}x{1}.png'.format(width, height))
-    except IOError:
-        print "cannot open mask file {0}x{1}.png".format(width, height)
-        break
-    try:
-        base = Image.open('base/{0}x{1}.png'.format(width, height));
-    except IOError:
-        print "Cannot open base file {0}x{1}.png".format(width, height)
-        break
-    base.paste(photo, (0, 0, width, height), mask)
-    base.save('outfiles/' + infile, 'PNG')
+def process():
+    for infile in os.listdir('infiles/'):
+        try:
+            photo = Image.open('infiles/' + infile)
+        except IOError:
+            print "Cannot open", infile
+            break
+        (width, height) = photo.size
+        try:
+            mask = Image.open('mask/{0}x{1}.png'.format(width, height))
+        except IOError:
+            print "cannot open mask file {0}x{1}.png".format(width, height)
+            break
+        try:
+            base = Image.open('base/{0}x{1}.png'.format(width, height));
+        except IOError:
+            print "Cannot open base file {0}x{1}.png".format(width, height)
+            break
+        base.paste(photo, (0, 0, width, height), mask)
+        base.save('outfiles/' + infile, 'PNG')
+
+createMask(400,300)
