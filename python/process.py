b162147f43353a947d0e1ade799d180a9a7e7ad7
Bill Ruddock <bill.ruddock@gmail.com>
2011-11-09 00:22:23 +0000

    process one file only, add test code in main

--- python/process.py
+++ python/process.py
@@ -6,24 +6,27 @@ import Image, ImageFile
 import math
 from fade import Fade
 
-def process():
+def process(infile, group_name):
+    try:
+        photo = Image.open('infiles/' + infile)
+    except IOError:
+        print "Cannot open", infile
+        break
+    (width, height) = photo.size
+    fade = Fade();
+    try:
+        mask = fade.getMask(width, height)
+    except IOError:
+        print "Cannot open mask file"
+        break
+    try:
+        base = fade.getBase(width, height)
+    except IOError:
+        print "Cannot open base file"
+        break
+    base.paste(photo, (0, 0, width, height), mask)
+    base.save('outfiles/' + infile, 'PNG')
+
+if __name__ == __main__:
     for infile in os.listdir('infiles/'):
-        try:
-            photo = Image.open('infiles/' + infile)
-        except IOError:
-            print "Cannot open", infile
-            break
-        (width, height) = photo.size
-        fade = Fade();
-        try:
-            mask = fade.getMask(width, height)
-        except IOError:
-            print "Cannot open mask file"
-            break
-        try:
-            base = fade.getBase(width, height)
-        except IOError:
-            print "Cannot open base file"
-            break
-        base.paste(photo, (0, 0, width, height), mask)
-        base.save('outfiles/' + infile, 'PNG')
+        process(infile, 'Test Group')
