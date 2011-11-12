d2c25d1cdf5b60fc3b716e467b3a76a0b5a9e737
Bill Ruddock <bill.ruddock@gmail.com>
2011-11-12 18:00:18 +0000

    add title drafts

--- python/process.py
+++ python/process.py
@@ -3,17 +3,18 @@
 import os, sys
 import shutil
 import Image, ImageFile
+import ImageFont, ImageDraw
 import math
 from fade import Fade
 
-def process(infile, group_name):
+def apply_mask(infile):
     try:
         photo = Image.open('infiles/' + infile)
     except IOError:
         print "Cannot open", infile
         return
     (width, height) = photo.size
-    fade = Fade();
+    fade = Fade()
     try:
         mask = fade.getMask(width, height)
     except IOError:
@@ -27,6 +28,22 @@ def process(infile, group_name):
     base.paste(photo, (0, 0, width, height), mask)
     base.save('outfiles/' + infile, 'PNG')
 
+def create_title(group_name):
+    width = 600
+    height = 200
+    fade = Fade()
+    base = Image.new('RGBA', (width, height))
+    draw = ImageDraw.Draw(base)
+    titleFont = ImageFont.truetype('fonts/BookmanDemi.pfb', 36)
+    #smallFont = ImageFont.truetype('Helvetica', 10)
+    #titleFont = ImageFont.truetype('/usr/local/share/fonts/BEAVER_LODGE.ttf', 36)
+    draw.text((10, 10), "Christmas Experience", fill=(0,0,0,255), font=titleFont)
+    base.save('fonttest.png')
+
+def process(infile, group_name):
+    apply_mask(infile)
+
 if __name__ == "__main__":
-    for infile in os.listdir('infiles/'):
-        process(infile, 'Test Group')
+    #for infile in os.listdir('infiles/'):
+    #    process(infile, 'Test Group')
+    create_title("Test Group")
