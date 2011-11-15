c6807a8476e0d11d69c3670ed56a148444d4766a
Bill Ruddock <bill.ruddock@gmail.com>
2011-11-15 23:42:00 +0000

    centred text

--- python/process.py
+++ python/process.py
@@ -28,16 +28,30 @@ def apply_mask(infile):
     base.paste(photo, (0, 0, width, height), mask)
     base.save('outfiles/' + infile, 'PNG')
 
+def get_centre_left(image_width, draw, text, font):
+    """ Get left coordinate to draw the text centred. """
+    (width, height) = draw.textsize(text, font=font)
+    left = (image_width - width) / 2
+    return left
+
 def create_title(group_name):
     width = 600
     height = 200
+    title = "Christmas Experience"
+    subtitle = "Bowley 2011"
     fade = Fade()
-    base = Image.new('RGBA', (width, height))
+    base = Image.new('RGBA', (width, height), (255,255,255,255))
     draw = ImageDraw.Draw(base)
     titleFont = ImageFont.truetype('fonts/BookmanDemi.pfb', 36)
-    #smallFont = ImageFont.truetype('Helvetica', 10)
-    #titleFont = ImageFont.truetype('/usr/local/share/fonts/BEAVER_LODGE.ttf', 36)
-    draw.text((10, 10), "Christmas Experience", fill=(0,0,0,255), font=titleFont)
+    smallFont = ImageFont.truetype('fonts/DejaVuSans.ttf', 10)
+    title_left = get_centre_left(width, draw, title, titleFont)
+    subtitle_left = get_centre_left(width, draw, subtitle, titleFont)
+    group_name_left = get_centre_left(width, draw, group_name, smallFont)
+    draw.text((title_left + 1, 11), title, fill=(0,255,0,255), font=titleFont)
+    draw.text((title_left, 10), title, fill=(255,0,0,255), font=titleFont)
+    draw.text((subtitle_left + 1, 51), subtitle, fill=(0,255,0,255), font=titleFont)
+    draw.text((subtitle_left, 50), subtitle, fill=(255,0,0,255), font=titleFont)
+    draw.text((group_name_left, 100), group_name, fill=(0,0,0,255), font=smallFont)
     base.save('fonttest.png')
 
 def process(infile, group_name):
