fd5698b0b2b3be595a61dff634419af167e22430
Bill Ruddock <bill.ruddock@gmail.com>
2011-12-15 14:45:16 +0000

    add reprint.py

---
+++ python/reprint.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""
+Reprint photos
+
+Usage: reprint filename copies
+"""
+import sys
+import tools
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    print "Usage: %s filename copies" % sys.argv[0]
+    sys.exit(1)
+if len(sys.argv) > 2:
+    copies = sys.argv[2]
+else:
+    copies = 1
+
+tools.print_image(filename, copies)
