7d29777dac64d68b920ae909c9ddb52e44c7c570
Bill Ruddock <bill.ruddock@gmail.com>
2012-11-24 14:34:29 +0000

    add CLI

---
+++ python/cli.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Command Line Interface to Bowley Christmas Experience
+import os
+import sys
+import devices
+from devices.device import Device
+import process
+import calendar
+
+def main(calendar=False):
+	tools.Logger.setCallback(lambda x: print x)
+	if calendar:
+		calendar.run()
+	else:
+		process.run()
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		calendar = (re.match('^cal.*', sys.argv[1], re.I) != None)
+	main()
