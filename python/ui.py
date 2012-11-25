22395b0eaaf4166473d8b7a3978d263772267bcf
Bill Ruddock <bill.ruddock@gmail.com>
2012-11-25 11:23:41 +0000

    add open image

--- python/ui.py
+++ python/ui.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 import wx
+import wx.lib.imagebrowser
+import os
 import devices
 from devices.device import Device
 
@@ -13,7 +15,10 @@ class MainWindow(wx.Frame):
         self.Show(True)
 
     def createWidgets(self):
+        vert = wx.BoxSizer(wx.VERTICAL)
         horiz = wx.BoxSizer(wx.HORIZONTAL)
+        self.staticImage = wx.StaticBitmap(self, wx.ID_ANY)
+        self.staticImage.SetSize((600, 400))
         self.scheduleCtrl = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.processBtn = wx.Button(self, label="Process")
 
@@ -21,12 +26,14 @@ class MainWindow(wx.Frame):
 
         horiz.Add(self.processBtn, 1, wx.ALIGN_LEFT)
         horiz.Add(self.scheduleCtrl, 1, wx.EXPAND)
-        self.SetSizer(horiz)
+        vert.Add(horiz)
+        self.SetSizer(vert)
         self.Centre()
 
     def setupMenu(self):
         """ Setup menu bar. """
         fileMenu = wx.Menu()
+        menuOpen = fileMenu.Append(wx.ID_OPEN, "&Open", "Open Photo")
         menuExit = fileMenu.Append(wx.ID_EXIT, "E&xit", "Quit Sleigh Photos")
 
         helpMenu = wx.Menu()
@@ -38,6 +45,7 @@ class MainWindow(wx.Frame):
         menuBar.Append(helpMenu, "&Help")
 
         self.SetMenuBar(menuBar)
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
 
@@ -53,6 +61,22 @@ class MainWindow(wx.Frame):
 
     def OnExit(self, event):
         self.Close(True)
+
+    def OnOpen(self, event):
+        cwd = os.getcwd()
+        initial_dir = os.path.join(cwd, 'png')
+        dlg = wx.lib.imagebrowser.ImageDialog(self, initial_dir)
+        dlg.Centre()
+        if dlg.ShowModal() == wx.ID_OK:
+            self.LoadImage(dlg.GetFile())
+        dlg.Destroy()
+
+    def LoadImage(self, name):
+        image = wx.Image(name)
+        (width, height) = self.staticImage.GetSize()
+        image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+        bitmap = wx.BitmapFromImage(image)
+        self.staticImage.SetBitmap(bitmap)
 
     def OnProcess(self, event):
         process.process()
