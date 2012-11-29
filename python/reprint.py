634a6d90367d7e3043aa44082c317cf7f48940b1
Bill Ruddock <bill.ruddock@gmail.com>
2012-11-29 00:34:48 +0000

    Use __main__ start method

--- python/reprint.py
+++ python/reprint.py
@@ -9,14 +9,18 @@ Usage: reprint filename copies
 import sys
 import tools
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
-    print "Usage: %s filename copies" % sys.argv[0]
-    sys.exit(1)
-if len(sys.argv) > 2:
-    copies = sys.argv[2]
-else:
-    copies = 1
+def main():
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        print "Usage: %s filename copies" % sys.argv[0]
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        copies = sys.argv[2]
+    else:
+        copies = 1
 
-tools.print_image(filename, copies)
+    tools.print_image(filename, copies)
+
+if __name__ == '__main__':
+    main()
